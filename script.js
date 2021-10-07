348b00c329da4c132562568e6f9ee4ad9f6bfdef
gran0482 <gran0482@algonquinlive.com>
2021-10-07 12:02:21 -0400

    adding in js data

---
+++ script.js
@@ -0,0 +1,8 @@
+//Data
+const $header = document.getElementById('header')
+const $container = document.getElementById('container')
+const $footer = document.getElementById('footer')
+
+//Code
+
+//Run
