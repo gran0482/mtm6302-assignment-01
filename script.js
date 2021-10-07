50102408fb0ddf836ab83892c806a25e170e2668
gran0482 <gran0482@algonquinlive.com>
2021-10-07 13:13:53 -0400

    adding formatting to css and adding content to js

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ const $header = document.getElementById('header')
 const $container = document.getElementById('container')
 const $footer = document.getElementById('footer')
 
+const ListOfEmojis = ['emoji 1', 'emoji 1', 'emoji 1', 'emoji 1']
+
+
+
 //Code
 
 //Run
+
+
+//loop for creating the emojis
+// for (let i = 0; i < 101; i++) {
+//     console.log(i) // Logs 0 to 4
+//   }
+  
+//   let total = 0
+
+
+
+//header and footer text
+$header.innerHTML = 'Emoji Gallery'
+$footer.innerHTML += '© Ben Grant'
